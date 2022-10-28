ff08bdaabf1fd9e4bb04747771cf9106f495046b
drewmania <justinhot906@gmail.com>
2022-10-28 19:35:02 -0400

    first commit

---
+++ js/startUp.js
@@ -0,0 +1,4 @@
+let screenWidth = screen.width;
+let screenHeight = screen.height;
+document.documentElement.style.setProperty('--screenWidth', screenWidth+ 'px');
+document.documentElement.style.setProperty('--screenHeight', screenHeight + 'px');
